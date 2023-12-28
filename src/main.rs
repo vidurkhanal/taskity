3a5601caec9e633e9200e263548a34a6405496e0
vidurkhanal <vkhanal@caldwell.edu>
2023-12-27 19:29:48 -0500

    chore: copied the ratatui templ

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+mod app;
+mod event;
+mod tui;
+mod ui;
+mod update;
+
+use app::App;
+use color_eyre::eyre::Result;
+use event::{Event, EventHandler};
+use ratatui::{prelude::CrosstermBackend, Terminal};
+use tui::Tui;
+use update::update;
+
+fn main() -> Result<()> {
+    // Create an application.
+    let mut app = App::new();
+
+    // Initialize the terminal user interface.
+    let backend = CrosstermBackend::new(std::io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.enter()?;
+
+    // Start the main loop.
+    while !app.should_quit {
+        // Render the user interface.
+        tui.draw(&mut app)?;
+        // Handle events.
+        match tui.events.next()? {
+            Event::Tick => {}
+            Event::Key(key_event) => update(&mut app, key_event),
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        };
+    }
+
+    // Exit the user interface.
+    tui.exit()?;
+    Ok(())
+}
