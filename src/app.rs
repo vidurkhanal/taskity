d1ed9fde8144d324bf51bf8263dd3fc67b8ba1a8
vidurkhanal <vkhanal@caldwell.edu>
2023-12-29 11:51:42 -0500

    feat: switched to AtomicBool for quit and added System to context

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,35 @@
+use std::sync::{atomic::AtomicBool, Arc};
+
+use sysinfo::System;
+
 pub struct App {
-    pub should_quit: bool,
+    pub should_quit: Arc<AtomicBool>,
     pub context: AppContext,
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default)]
 pub struct AppContext {
     pub tab_index: usize,
     pub row_index: usize,
+    pub system: System,
 }
 
 impl App {
     /// Constructs a new instance of [`App`].
-    pub fn new() -> Self {
+    pub fn new(should_quit: Arc<AtomicBool>) -> Self {
         Self {
-            should_quit: false,
+            should_quit,
             context: AppContext {
                 tab_index: 0,
                 row_index: 0,
+                system: System::new_all(),
             },
         }
     }
 
     /// Set should_quit to true to quit the application.
     pub fn quit(&mut self) {
-        self.should_quit = true;
+        self.should_quit
+            .store(true, std::sync::atomic::Ordering::Relaxed);
     }
 }
